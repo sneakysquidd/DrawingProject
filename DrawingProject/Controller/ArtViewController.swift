cedc33ddba224cde578315aa628c87733e06c88b
_gavbob <gsct2002@gmail.com>
2018-11-01 12:15:28 -0600

    Added segue viewcontroller and adjusted ArtViewController to be public

--- DrawingProject/Controller/ArtViewController.swift
+++ DrawingProject/Controller/ArtViewController.swift
@@ -8,16 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class ArtViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }

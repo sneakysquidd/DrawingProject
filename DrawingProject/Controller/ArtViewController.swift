477369fda247a906ef2ae8ff4a3a8557ccb873db
_gavbob <gsct2002@gmail.com>
2018-11-01 11:49:25 -0600

    Organized files into folders and changed viewcontroller's name

---
+++ DrawingProject/Controller/ArtViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawingProject
+//
+//  Created by Thomas, Gavin on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

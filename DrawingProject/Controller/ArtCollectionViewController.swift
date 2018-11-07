288ae74ffa59be1cfb58d33dd8999124cdd9c7b1
_gavbob <gsct2002@gmail.com>
2018-11-07 13:02:23 -0700

    Created collectionView method that gives size and padding for cells

--- DrawingProject/Controller/ArtCollectionViewController.swift
+++ DrawingProject/Controller/ArtCollectionViewController.swift
@@ -16,7 +16,7 @@ public class ArtCollectionViewController: UICollectionViewController
     //MARK: Data members for Creativity Screen
     
     private let sectionInsets = UIEdgeInsets(top: 50.0, left:
-        20.0, bottom: 50.0, right: 20.0)
+        25.0, bottom: 50.0, right: 25.0)
     private let itemsPerRowCompact : CGFloat = 4
     private let itemsPerRowNormal : CGFloat = 6
     
@@ -34,6 +34,19 @@ public class ArtCollectionViewController: UICollectionViewController
             ]
     }()
     
+    private let labels : [String] =
+    {
+        return [
+        "My Octocat",
+        "Swift Haiku",
+        "Minecraft",
+        "Java Haiku",
+        "Monsieur Krabs",
+        "Mainframe Haiku",
+        "Best Smash Character",
+        "Swift Drawing"
+        ]
+    }()
     
     public override func viewDidLoad() -> Void
     {
@@ -63,15 +76,13 @@ public class ArtCollectionViewController: UICollectionViewController
 
     public override func numberOfSections(in collectionView: UICollectionView) -> Int
     {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return creativeCS.count
     }
 
     public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
@@ -85,13 +96,19 @@ public class ArtCollectionViewController: UICollectionViewController
 
     // MARK: UICollectionViewDelegate
 
-    /*
+    
     // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool
-     {
-        return true
+    public func collectionView(_ collectionView: UICollectionView,
+                                 layout collectionViewLayout: UICollectionViewLayout,
+                                 sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRowCompact
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
     }
-    */
+   
 
     /*
     // Uncomment this method to specify if the specified item should be selected
